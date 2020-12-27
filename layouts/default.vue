09a431eb247639d138bdc4b925d497586e4453bd
Christian Paranas <christiannparanas@gmail.com>
2020-12-27 20:26:20 +0800

    init nav head

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <Nav />
+    <div class="content">
+      <div class="header text-3xl">
+        Find Movies, Tv shows and more ...
+      </div>
+      <div class="search-bar">
+        <input type="text" placeholder="Search" >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+  body {
+    background-color: #091a28;
+    color: #fff;
+  }
+
+  .container {
+
+    .content {
+      padding: 100px 30px 30px;
+
+      .search-bar {
+        margin-top: 20px;
+
+        input {
+          width: 100%;
+          padding: 20px;
+          border-radius: 20px;
+          background-color: #1f2937;
+          outline: none;
+        }
+      }
+
+    }
+  }
+
+</style>
