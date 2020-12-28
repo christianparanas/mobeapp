4a49b7c96fa3b01a82c0177b3e04886923003953
Christian Paranas <christiannparanas@gmail.com>
2020-12-28 23:37:07 +0800

    slug page cast

---
+++ components/CastPoster.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="CastPoster" class="" v-if="havePoster">
+		<img :src="image" alt="">
+		<div class="name">{{ content.name }}</div>
+	</div>
+</template>
+
+<script>
+  export default {
+	name: 'CastPoster',
+    props: ['content'],
+    data() {
+    	return {
+    		havePoster: true,
+    		image: ''
+    	}
+    },
+    mounted() {
+   	// checks if the movie have a poster, if not, it will not be rendered
+			if(this.content.profile_path == null) {
+				this.havePoster = false
+			} else {
+				this.image = `https://image.tmdb.org/t/p/w342/${this.content.profile_path}`
+			}
+   	}
+  }
+</script>
+
+<style>
+	
+</style>
--- pages/_slug.vue
+++ pages/_slug.vue
@@ -9,12 +9,36 @@
     			{{ genre.name }}
     			</div>
     		</div>
+    		<div class="overview">{{ contentData.overview }}</div>
+    		<div class="mini-infos">
+    			<div class="movie__release-date">
+    				<div class="">Release Date</div><div class="">{{ contentData.release_date }}</div>
+    			</div>
+    			<div class="movie__release-date">
+    				<div class="">Status</div><div class="">{{ contentData.status }}</div>
+    			</div>
+    			<div class="movie__release-date">
+    				<div class="">Rating</div><div class="">{{ contentData.vote_average }}</div>
+    			</div>
+    			<div class="movie__release-date">
+    				<div class="">Runtime</div><div class="">{{ contentData.runtime }}min</div>
+    			</div>
+    		</div>
+    		<div class="casts">
+    			<div class="title">
+    				<div class="">Cast</div>
+    				<div class="movie__see_all_cast">See All</div></div>
+    			<div class="castPosterWrapper">
+    				<CastPoster v-for="cast in topCast" :content="cast" :key="cast.cast_id" />
+    			</div>
+    		</div>
     	</div>
   	</div>
 
-    <NuxtLink to="/">
+<!--     <NuxtLink to="/">
     	<p>Go back</p>
-    </NuxtLink>
+    </NuxtLink> -->
+
   </div>
 </template>
 <script>
@@ -26,11 +50,24 @@
 
       const genres = contentData.genres
       const contentBdImg = `https://image.tmdb.org/t/p/w342/${contentData.backdrop_path}`
+      const casts = await $axios.$get(`https://api.themoviedb.org/3/movie/${parseInt(params.slug)}/credits?api_key=a807f0095433ac989503323b5b0bc933&language=en-US`)
+
+
+      // filter the array, limit items 10
+      let i = 0
+      const topCast = []
+      for(i; i < 10; i++) {
+      	topCast.push(casts.cast[i])
+      }
+
+      console.log(topCast)
+
 
       return { 
       	contentData,
       	contentBdImg,
-      	genres
+      	genres,
+      	topCast
       }
     },
   }
@@ -38,6 +75,9 @@
 
 <style lang="scss">
 	.slug {
+		width: 100%;
+    background-color: #091a28;
+    color: #fff;
 
 		.slug-head {
 
@@ -49,10 +89,8 @@
 				padding: 10px 20px;
 
 				.movie_title {
-					font-size: 25px;
-					background: rgba(0, 0, 0, 0.2);
+					font-size: 30px;
 					width: fit-content;
-					padding: 5px 10px;
 					position: relative;
 					top: -30px;
 				}
@@ -61,15 +99,54 @@
 				.genres-wrapper {
 					display: flex;
 					flex-direction: row;
-					padding-left: 15px;
 					position: relative;
-					top: -20px;
+					top: -25px;
 
 					.genres {
 						margin-right: 10px;
 						color: orange;
 						font-size: 12px;
 					}
+				}
+
+				.overview {
+					font-size: 13px;
+				}
+
+				.mini-infos {
+					display: grid;
+					margin-top: 30px;
+					font-size: 13px;
+					grid-row-gap: 10px;
+					grid-template-columns: 1fr 1fr 1fr;
+				}
+
+				.casts {
+					margin-top: 30px;
+
+					.title {
+						font-size: 20px;
+						display: flex;
+						justify-content: space-between;
+
+						.movie__see_all_cast {
+							color: orange;
+						}
+					}
+
+					.castPosterWrapper {
+						margin-top: 10px;
+        		display: grid;
+        		grid-template-columns: repeat(20, 150px);
+        		grid-column-gap: 8px;
+        		overflow-x: scroll;
+
+        		&::-webkit-scrollbar {
+          		width: 0px;
+          		background: transparent;
+        		}
+					}
+
 				}
 			}
 		}
